c9f5436b7b6276a272bdae51b6da5858845458a8
lixfz <lixf@sovon.net>
2020-11-05 10:13:26 +0800

    make run_huypergbm compatible with multiprocess

--- tests/dask_/run_hypergbm_dask.py
+++ tests/dask_/run_hypergbm_dask.py
@@ -14,35 +14,41 @@ from hypernets.core.searcher import OptimizeDirection
 from hypernets.searchers.random_searcher import RandomSearcher
 from tests import test_output_dir
 
-# client = Client("tcp://127.0.0.1:64958")
-client = Client(processes=False, threads_per_worker=2, n_workers=1, memory_limit='4GB')
-print(client)
 
-rs = RandomSearcher(get_space_num_cat_pipeline_complex, optimize_direction=OptimizeDirection.Maximize)
-hk = HyperGBM(rs, task='classification', reward_metric='accuracy',
-              cache_dir=f'{test_output_dir}/hypergbm_cache',
-              callbacks=[SummaryCallback(), FileLoggingCallback(rs, output_dir=f'{test_output_dir}/hyn_logs')])
+def main():
+    # client = Client("tcp://127.0.0.1:64958")
+    client = Client(processes=False, threads_per_worker=2, n_workers=1, memory_limit='4GB')
+    print(client)
 
-df = dsutils.load_bank_by_dask()
-df.drop(['id'], axis=1)
-df['y'] = dm_pre.LabelEncoder().fit_transform(df['y'])
-# df = df.sample(frac=0.1)
+    rs = RandomSearcher(get_space_num_cat_pipeline_complex, optimize_direction=OptimizeDirection.Maximize)
+    hk = HyperGBM(rs, task='classification', reward_metric='accuracy',
+                  cache_dir=f'{test_output_dir}/hypergbm_cache',
+                  callbacks=[SummaryCallback(), FileLoggingCallback(rs, output_dir=f'{test_output_dir}/hyn_logs')])
 
-# object_columns = [i for i, v in df.dtypes.items() if v == 'object']
-# for c in object_columns:
-#     df[c] = df[c].astype('category')
-# df = df.categorize(object_columns)
+    df = dsutils.load_bank_by_dask()
+    df.drop(['id'], axis=1)
+    df['y'] = dm_pre.LabelEncoder().fit_transform(df['y'])
+    # df = df.sample(frac=0.1)
 
-X_train, X_test = train_test_split(df, test_size=0.8, random_state=42)
-y_train = X_train.pop('y')
-y_test = X_test.pop('y')
+    # object_columns = [i for i, v in df.dtypes.items() if v == 'object']
+    # for c in object_columns:
+    #     df[c] = df[c].astype('category')
+    # df = df.categorize(object_columns)
 
-hk.search(X_train, y_train, X_test, y_test, max_trails=50)
-print('-' * 30)
+    X_train, X_test = train_test_split(df, test_size=0.8, random_state=42)
+    y_train = X_train.pop('y')
+    y_test = X_test.pop('y')
 
-best_trial = hk.get_best_trail()
-print(f'best_train:{best_trial}')
-estimator = hk.final_train(best_trial.space_sample, X_train, y_train)
-score = estimator.predict(X_test)
-result = estimator.evaluate(X_test, y_test, metrics=['accuracy', 'auc', 'logloss'])
-print(f'final result:{result}')
+    hk.search(X_train, y_train, X_test, y_test, max_trails=50)
+    print('-' * 30)
+
+    best_trial = hk.get_best_trail()
+    print(f'best_train:{best_trial}')
+    estimator = hk.final_train(best_trial.space_sample, X_train, y_train)
+    score = estimator.predict(X_test)
+    result = estimator.evaluate(X_test, y_test, metrics=['accuracy', 'auc', 'logloss'])
+    print(f'final result:{result}')
+
+
+if __name__ == '__main__':
+    main()
