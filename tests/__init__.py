06d91893b993fbcb31cf71b63f83f548531c4233
lixfz <lixf@sovon.net>
2020-11-30 11:54:53 +0800

    Change pytest storage root to system temporary directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,5 +9,9 @@ import os
 import time
 
 test_output_dir = tempfile.mkdtemp(prefix=time.strftime("hygbm_test_%m%d%H%M_"))
+
+os.environ['HYN_STORAGE_TYPE'] = 'file'
+os.environ['HYN_STORAGE_ROOT'] = test_output_dir
+
 os.environ['DEEPTABLES_HOME'] = test_output_dir
 os.environ['HYPERNETS_HOME'] = test_output_dir
