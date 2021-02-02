e11efc6edff8ad254965be499feedcf49dabb712
lixfz <lixf@sovon.net>
2021-02-02 20:00:49 +0800

    Update search space

--- hypergbm/dask/search_space.py
+++ hypergbm/dask/search_space.py
@@ -7,8 +7,9 @@ from functools import partial
 from hypergbm.dask import dask_transformers as tf, dask_ops as ops
 from hypergbm.estimators import LightGBMDaskEstimator, CatBoostDaskEstimator, XGBoostDaskEstimator
 from hypergbm.pipeline import Pipeline, DataFrameMapper
+from hypergbm.search_space import GeneralSearchSpaceGenerator
 from hypernets.core.ops import ModuleChoice, HyperInput
-from hypernets.core.search_space import Choice, HyperSpace, Int
+from hypernets.core.search_space import Choice, HyperSpace, Int, get_default_space
 from hypernets.utils import logging
 from tabular_toolbox import dask_ex as dex
 from tabular_toolbox.column_selector import column_object, column_all
@@ -16,18 +17,104 @@ from tabular_toolbox.column_selector import column_object, column_all
 logger = logging.get_logger(__name__)
 
 
-def search_space_general(dataframe_mapper_default=False,
-                         lightgbm_init_kwargs=None,
-                         lightgbm_fit_kwargs=None,
-                         xgb_init_kwargs=None,
-                         xgb_fit_kwargs=None,
-                         catboost_init_kwargs=None,
-                         catboost_fit_kwargs=None,
-                         class_balancing=None,
-                         n_esitimators=200,
-                         early_stopping_rounds=None,
-                         enable_persist=True,
-                         **kwargs):
+def _transformer_decorater(cache, cache_key, remove_keys, transformer):
+    persister = tf.DataCacher(cache,
+                              name=f'{transformer.name}_persister',
+                              cache_key=cache_key,
+                              remove_keys=remove_keys,
+                              fit_transform=True,
+                              transform=True)
+    transformer = persister(transformer)
+
+    return transformer
+
+
+def _dfm_decorater(cache, cache_key, remove_keys, dfm):
+    persister = tf.DataCacher(cache,
+                              name=f'dfm_persister',
+                              cache_key=cache_key,
+                              remove_keys=remove_keys,
+                              fit_transform=True,
+                              transform=True)
+    pipeline = Pipeline([persister], name='dfm_cacher', columns=column_all)
+    return pipeline(dfm)
+
+
+class DaskGeneralSearchSpaceGenerator(GeneralSearchSpaceGenerator):
+
+    def __init__(self, enable_lightgbm=True, enable_xgb=True, enable_catboost=True, enable_persist=True, **kwargs):
+        super().__init__(**kwargs)
+
+        self.enable_lightgbm = enable_lightgbm
+        self.enable_xgb = enable_xgb
+        self.enable_catboost = enable_catboost
+
+        self.enable_persist = enable_persist
+
+    @property
+    def default_xgb_init_kwargs(self):
+        return {**super().default_xgb_init_kwargs, 'tree_method': 'approx'}
+
+    @property
+    def estimators(self):
+        r = {}
+
+        if self.enable_xgb:
+            r['xgb'] = (XGBoostDaskEstimator, self.default_xgb_init_kwargs, self.default_xgb_fit_kwargs)
+
+        if dex.is_local_dask():
+            if self.enable_lightgbm:
+                r['lightgbm'] = \
+                    (LightGBMDaskEstimator, self.default_lightgbm_init_kwargs, self.default_lightgbm_fit_kwargs)
+            if self.enable_catboost:
+                r['catboost'] = \
+                    (CatBoostDaskEstimator, self.default_catboost_init_kwargs, self.default_catboost_fit_kwargs)
+
+        return r
+
+    def create_preprocessor(self, hyper_input, **kwargs):
+        cat_pipeline_mode = kwargs.pop('cat_pipeline_mode', 'simple')
+        dataframe_mapper_default = kwargs.pop('dataframe_mapper_default', None)
+
+        if self.enable_persist:
+            space = get_default_space()
+            assert not hasattr(space, 'cache_')
+
+            cache = {}
+            setattr(space, 'cache_', cache)
+            dfm_decorater = partial(_dfm_decorater, cache, 'dfm', 'num,cat')
+            num_transformer_decorater = partial(_transformer_decorater, cache, 'num', 'dfm')
+            cat_transformer_decorater = partial(_transformer_decorater, cache, 'cat', 'dfm')
+        else:
+            dfm_decorater = ops.default_transformer_decorator
+            num_transformer_decorater = ops.default_transformer_decorator
+            cat_transformer_decorater = ops.default_transformer_decorator
+
+        num_pipeline = ops.numeric_pipeline_complex(decorate=num_transformer_decorater)(hyper_input)
+        cat_pipeline = ops.categorical_pipeline_complex(decorate=cat_transformer_decorater)(hyper_input)
+
+        dfm = DataFrameMapper(default=dataframe_mapper_default, input_df=True, df_out=True,
+                              df_out_dtype_transforms=[(column_object, 'int')])([num_pipeline, cat_pipeline])
+        preprocessor = dfm_decorater(dfm)
+
+        return preprocessor
+
+
+search_space_general = DaskGeneralSearchSpaceGenerator(n_estimators=200, enable_persist=False)
+
+
+def search_space_general_removed(dataframe_mapper_default=False,
+                                 lightgbm_init_kwargs=None,
+                                 lightgbm_fit_kwargs=None,
+                                 xgb_init_kwargs=None,
+                                 xgb_fit_kwargs=None,
+                                 catboost_init_kwargs=None,
+                                 catboost_fit_kwargs=None,
+                                 class_balancing=None,
+                                 n_esitimators=200,
+                                 early_stopping_rounds=None,
+                                 enable_persist=True,
+                                 **kwargs):
     assert dex.dask_enabled(), 'Dask client must be initialized.'
 
     if lightgbm_init_kwargs is None: lightgbm_init_kwargs = {}
--- hypergbm/search_space.py
+++ hypergbm/search_space.py
@@ -7,24 +7,197 @@ __author__ = 'yangjian'
 from hypergbm.estimators import LightGBMEstimator, XGBoostEstimator, CatBoostEstimator, HistGBEstimator
 from hypergbm.feature_generators import CrossCategorical
 from hypergbm.pipeline import DataFrameMapper, Pipeline
-from hypergbm.sklearn.sklearn_ops import numeric_pipeline_simple, numeric_pipeline_complex, categorical_pipeline_simple, \
-    categorical_pipeline_complex
-from hypergbm.sklearn.transformers import FeatureGenerationTransformer, PolynomialFeatures
+from hypergbm.sklearn.sklearn_ops import numeric_pipeline_simple, numeric_pipeline_complex, \
+    categorical_pipeline_simple, categorical_pipeline_complex
+from hypergbm.sklearn.transformers import FeatureGenerationTransformer
 from hypernets.core.ops import ModuleChoice, HyperInput
-from hypernets.core.search_space import Choice, Real, Int, Bool
+from hypernets.core.search_space import Choice, Real, Int
 from hypernets.core.search_space import HyperSpace
-from tabular_toolbox.column_selector import column_object, column_exclude_datetime, column_number
+from tabular_toolbox.column_selector import column_object, column_exclude_datetime
 
 
-def search_space_general(dataframe_mapper_default=False,
-                         lightgbm_fit_kwargs=None,
-                         xgb_fit_kwargs=None,
-                         catboost_fit_kwargs=None,
-                         histgb_fit_kwargs=None,
-                         cat_pipeline_mode='simple',
-                         class_balancing=None,
-                         n_esitimators=200,
-                         **kwargs):
+def _merge_dict(*args):
+    d = {}
+    for a in args:
+        if isinstance(a, dict):
+            d.update(a)
+    return d
+
+
+class _HyperEstimatorCreator(object):
+    def __init__(self, cls, init_kwargs, fit_kwargs):
+        super(_HyperEstimatorCreator, self).__init__()
+
+        self.estimator_cls = cls
+        self.estimator_fit_kwargs = fit_kwargs if fit_kwargs is not None else {}
+        self.estimator_init_kwargs = init_kwargs if init_kwargs is not None else {}
+
+    def __call__(self, *args, **kwargs):
+        return self.estimator_cls(self.estimator_fit_kwargs, **self.estimator_init_kwargs)
+
+
+class SearchSpaceGenerator(object):
+    def __init__(self, **kwargs) -> None:
+        super().__init__()
+
+        self.kwargs = kwargs
+
+    def create_preprocessor(self, hyper_input, **kwargs):
+        raise NotImplementedError()
+
+    def create_estimators(self, hyper_input, **kwargs):
+        raise NotImplementedError()
+
+    def __call__(self, *args, **kwargs):
+        kwargs = _merge_dict(self.kwargs, kwargs)
+
+        space = HyperSpace()
+        with space.as_default():
+            hyper_input = HyperInput(name='input1')
+            self.create_estimators(self.create_preprocessor(hyper_input, **kwargs), **kwargs)
+            space.set_inputs(hyper_input)
+
+        return space
+
+
+class BaseSearchSpaceGenerator(SearchSpaceGenerator):
+    @property
+    def estimators(self):
+        # return dict:  key-->(hyper_estimator_cls, default_init_kwargs, default_fit_kwargs)
+        raise NotImplementedError()
+
+    def create_preprocessor(self, hyper_input, **kwargs):
+        cat_pipeline_mode = kwargs.pop('cat_pipeline_mode', 'simple')
+        dataframe_mapper_default = kwargs.pop('dataframe_mapper_default', None)
+
+        num_pipeline = numeric_pipeline_complex()(hyper_input)
+        if cat_pipeline_mode == 'simple':
+            cat_pipeline = categorical_pipeline_simple()(hyper_input)
+        else:
+            cat_pipeline = categorical_pipeline_complex()(hyper_input)
+
+        preprocessor = DataFrameMapper(default=dataframe_mapper_default, input_df=True, df_out=True,
+                                       df_out_dtype_transforms=[(column_object, 'int')])([num_pipeline, cat_pipeline])
+
+        return preprocessor
+
+    def create_estimators(self, hyper_input, **kwargs):
+        assert len(self.estimators.keys()) > 0
+
+        creators = [_HyperEstimatorCreator(pairs[0],
+                                           init_kwargs=_merge_dict(pairs[1], kwargs.pop(f'{k}_init_kwargs', None)),
+                                           fit_kwargs=_merge_dict(pairs[2], kwargs.pop(f'{k}_fit_kwargs', None)))
+                    for k, pairs in self.estimators.items()]
+
+        for c in creators:
+            for k, v in kwargs.items():
+                if k in c.estimator_init_kwargs.keys():
+                    c.estimator_init_kwargs[k] = v
+
+        estimators = [c() for c in creators]
+        return ModuleChoice(estimators, name='estimator_options')(hyper_input)
+
+
+class GeneralSearchSpaceGenerator(BaseSearchSpaceGenerator):
+    def __init__(self, enable_lightgbm=True, enable_xgb=True, enable_catboost=True, enable_histgb=True, **kwargs):
+        super(GeneralSearchSpaceGenerator, self).__init__(**kwargs)
+
+        self.enable_lightgbm = enable_lightgbm
+        self.enable_xgb = enable_xgb
+        self.enable_catboost = enable_catboost
+        self.enable_histgb = enable_histgb
+
+    @property
+    def default_xgb_init_kwargs(self):
+        return {
+            'booster': Choice(['gbtree', 'dart']),
+            'max_depth': Choice([3, 5, 7, 10]),
+            'n_estimators': Choice([10, 30, 50, 100, 200, 300]),
+            'learning_rate': Choice([0.001, 0.01, 0.5, 0.1]),
+            'min_child_weight': Choice([1, 5, 10]),
+            'gamma': Choice([0.5, 1, 1.5, 2, 5]),
+            'reg_alpha': Choice([0.001, 0.01, 0.1, 1, 10, 100]),
+            'reg_lambda': Choice([0.001, 0.01, 0.1, 0.5, 1]),
+            'class_balancing': None,
+        }
+
+    @property
+    def default_xgb_fit_kwargs(self):
+        return {}
+
+    @property
+    def default_lightgbm_init_kwargs(self):
+        return {
+            'n_estimators': Choice([10, 30, 50, 100, 200, 300]),
+            'boosting_type': Choice(['gbdt', 'dart', 'goss']),
+            'num_leaves': Int(15, 513, 5),
+            'learning_rate': Choice([0.001, 0.01, 0.5, 0.1]),
+            'max_depth': Choice([3, 5, 7, 10]),
+            'reg_alpha': Choice([0.001, 0.01, 0.1, 1, 10, 100]),
+            'reg_lambda': Choice([0.001, 0.01, 0.1, 0.5, 1]),
+            'class_balancing': None,
+        }
+
+    @property
+    def default_lightgbm_fit_kwargs(self):
+        return {}
+
+    @property
+    def default_catboost_init_kwargs(self):
+        return {
+            'silent': True,
+            'n_estimators': Choice([10, 30, 50, 100, 200, 300]),
+            'depth': Choice([3, 5, 7, 10]),
+            'learning_rate': Choice([0.001, 0.01, 0.5, 0.1]),
+            'l2_leaf_reg': Choice([None, 2, 10, 20, 30]),
+            'class_balancing': None,
+        }
+
+    @property
+    def default_catboost_fit_kwargs(self):
+        return {}
+
+    @property
+    def default_histgb_init_kwargs(self):
+        return {
+            'learning_rate': Choice([0.01, 0.1, 0.2, 0.5, 0.8, 1]),
+            'min_samples_leaf': Choice([10, 20, 50, 80, 100, 150, 180, 200]),
+            'max_leaf_nodes': Int(15, 513, 5),
+            'l2_regularization': Choice([1e-10, 1e-8, 1e-6, 1e-5, 1e-3, 0.01, 0.1, 1])
+        }
+
+    @property
+    def default_histgb_fit_kwargs(self):
+        return {}
+
+    @property
+    def estimators(self):
+        r = {}
+
+        if self.enable_lightgbm:
+            r['lightgbm'] = (LightGBMEstimator, self.default_lightgbm_init_kwargs, self.default_lightgbm_fit_kwargs)
+        if self.enable_xgb:
+            r['xgb'] = (XGBoostEstimator, self.default_xgb_init_kwargs, self.default_xgb_fit_kwargs)
+        if self.enable_catboost:
+            r['catboost'] = (CatBoostEstimator, self.default_catboost_init_kwargs, self.default_catboost_fit_kwargs)
+        if self.enable_histgb:
+            r['histgb'] = (HistGBEstimator, self.default_histgb_init_kwargs, self.default_histgb_fit_kwargs)
+
+        return r
+
+
+search_space_general = GeneralSearchSpaceGenerator(enable_histgb=False, n_estimators=200)
+
+
+def search_space_general_removed(dataframe_mapper_default=False,
+                                 lightgbm_fit_kwargs=None,
+                                 xgb_fit_kwargs=None,
+                                 catboost_fit_kwargs=None,
+                                 histgb_fit_kwargs=None,
+                                 cat_pipeline_mode='simple',
+                                 class_balancing=None,
+                                 n_esitimators=200,
+                                 **kwargs):
     """
     A general search space function
 
