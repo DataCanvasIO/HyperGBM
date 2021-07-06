d38ef75707b316bbc8d5c35ccac32a32aa4adfd8
lixfz <lixf@sovon.net>
2021-07-06 20:16:16 +0800

    Update test settings

--- hypergbm/tests/__init__.py
+++ hypergbm/tests/__init__.py
@@ -8,6 +8,10 @@ import os
 import tempfile
 import time
 
+from hypernets.tabular.cfg import TabularCfg
+
+TabularCfg.tfidf_primitive_output_feature_count = 5
+
 test_output_dir = tempfile.mkdtemp(prefix=time.strftime("hygbm_test_%m%d%H%M_"))
 
 os.environ['DEEPTABLES_HOME'] = test_output_dir
