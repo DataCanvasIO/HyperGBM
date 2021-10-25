6412cfb0a6c26d2ec29940a75786e4ab618afd98
Enpen <1172462905@qq.com>
2021-10-25 13:57:07 +0800

    Update estimators.py

--- hypergbm/estimators.py
+++ hypergbm/estimators.py
@@ -336,10 +336,10 @@ class XGBEstimatorMixin:
         return callback
 
     def prepare_fit_kwargs(self, X, y, kwargs):
-        task = self.__dict__.get('task')
-        if kwargs.get('eval_metric') is None:
-            if task is not None and task == const.TASK_MULTICLASS:
-                kwargs['eval_metric'] = 'mlogloss'
+#         task = self.__dict__.get('task')
+#         if kwargs.get('eval_metric') is None:
+#             if task is not None and task == const.TASK_MULTICLASS:
+#                 kwargs['eval_metric'] = 'mlogloss'
         if kwargs.get('early_stopping_rounds') is None and kwargs.get('eval_set') is not None:
             kwargs['early_stopping_rounds'] = _default_early_stopping_rounds(self)
         return kwargs
