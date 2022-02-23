fb5d56a0a3ef5cd4c61de703c3d4e03d3c4f2564
Haifeng Wu <oaksharks@outlook.com>
2022-02-23 17:37:57 +0800

    Set line separator to \n and update version

--- hypergbm/experiment_callbacks/_experiment.py
+++ hypergbm/experiment_callbacks/_experiment.py
@@ -6,14 +6,8 @@ logger = hyn_logging.get_logger(__name__)
 
 
 def create_notebook_experiment_callback():
-    try:
-        from experiment_notebook_widget.callbacks import NotebookExperimentCallback
-        return NotebookExperimentCallback(HyperGBMNotebookHyperModelCallback)
-    except Exception as e:
-        logger.warning("No notebook visualization module detected, please install by command:"
-                       "\"pip install experiment-notebook-widget\"")
-        logger.exception(e)
-        return None
+    from experiment_notebook_widget.callbacks import NotebookExperimentCallback
+    return NotebookExperimentCallback(HyperGBMNotebookHyperModelCallback)
 
 
 def create_web_vis_experiment_callback(**kwargs):
