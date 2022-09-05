9f1d4588456a2d583b5d5ef0af3883cd84874990
lixfz <lixf@sovon.net>
2022-09-05 13:32:23 +0800

    Upgrade to 0.2.5.5

--- setup.py
+++ setup.py
@@ -83,6 +83,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development',
