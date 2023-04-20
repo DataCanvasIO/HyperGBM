652ae0c6184f01b3a2cb4c2ff3bdae94ca1fb654
Haifeng Wu <oaksharks@163.com>
2023-04-20 12:06:22 +0800

    compatible with newer sklearn

--- hypergbm/dask/dask_transformers.py
+++ hypergbm/dask/dask_transformers.py
@@ -2,7 +2,7 @@
 """
 
 """
-
+import inspect
 import numpy as np
 from dask_ml import impute as dimp
 from dask_ml import preprocessing as dm_pre
@@ -36,7 +36,7 @@ class StandardScaler(HyperTransformer):
 
 
 class SafeOneHotEncoder(HyperTransformer):
-    def __init__(self, categories='auto', drop=None, sparse_output=True, dtype=np.float64, space=None,
+    def __init__(self, categories='auto', drop=None, dtype=np.float64, space=None,
                  name=None, **kwargs):
         if categories is not None and categories != 'auto':
             kwargs['categories'] = categories
@@ -44,8 +44,14 @@ class SafeOneHotEncoder(HyperTransformer):
         #     kwargs['categories'] = 'object'
         if drop is not None:
             kwargs['drop'] = drop
-        if sparse_output is not None and sparse_output != True:
-            kwargs['sparse_output'] = sparse_output
+
+        keys = set(inspect.signature(dm_pre.OneHotEncoder.__init__).parameters.keys())
+        if 'sparse_output' in keys:
+            # above sklearn 1.2
+            kwargs['sparse_output'] = False
+        else:
+            kwargs['sparse'] = False
+
         if dtype is not None and dtype != True:
             kwargs['dtype'] = dtype
         # if handle_unknown is not None and handle_unknown != 'error':
