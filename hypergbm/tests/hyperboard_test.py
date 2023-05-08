ccda183b920293d3bd6056588bf44ca414bd14ec
Haifeng Wu <oaksharks@163.com>
2023-05-08 18:47:55 +0800

    speedup test

--- hypergbm/tests/hyperboard_test.py
+++ hypergbm/tests/hyperboard_test.py
@@ -10,7 +10,6 @@ from hypergbm.experiment_callbacks import create_web_vis_experiment_callback, cr
     create_notebook_hyper_model_callback, create_web_vis_hyper_model_callback
 from hypergbm import make_experiment
 
-
 logger = logging.get_logger(__name__)
 
 
@@ -23,8 +22,8 @@ def _notebook_widget_and_web_app_ready():
         return False
 
 
-deps_ready = pytest.mark.skipif(not _notebook_widget_and_web_app_ready(),
-                                reason='hboard or hboard-widget not installed')
+need_hboard = pytest.mark.skipif(not _notebook_widget_and_web_app_ready(),
+                                 reason='hboard or hboard-widget not installed')
 
 
 def _run_experiment(creator):
@@ -36,9 +35,8 @@ def _run_experiment(creator):
     exp = creator(maker=make_experiment,
                   callbacks=[webui_callback, nb_callback],
                   search_callbacks=[webui_model_callback, nb_model_callback])
-    estimator = exp.run(max_trials=10)
+    estimator = exp.run(max_trials=3)
     assert estimator
-    
     logfile = webui_callback.get_log_file(exp)
     assert logfile
     with open(logfile, 'r', newline='\n') as f:
@@ -49,51 +47,51 @@ def _run_experiment(creator):
     return events
 
 
-@deps_ready
+@need_hboard
 def test_data_clean():
     _run_experiment(experiment_factory.create_data_clean_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_drift_detection():
     _run_experiment(experiment_factory.create_drift_detection_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_multicollinearity_detect():
     _run_experiment(experiment_factory.create_multicollinearity_detect_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_feature_generation():
     _run_experiment(experiment_factory.create_feature_generation_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_feature_reselection_experiment():
     _run_experiment(experiment_factory.create_feature_reselection_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_feature_selection_experiment():
     _run_experiment(experiment_factory.create_feature_selection_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_pseudo_labeling_experiment():
     _run_experiment(experiment_factory.create_pseudo_labeling_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_disable_cv():
     _run_experiment(experiment_factory.create_disable_cv_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_custom_metric_func():
     _run_experiment(experiment_factory.create_custom_reward_metric_func_experiment)
 
 
-@deps_ready
+@need_hboard
 def test_custom_metric_class():
     _run_experiment(experiment_factory.create_custom_reward_metric_class_experiment)
